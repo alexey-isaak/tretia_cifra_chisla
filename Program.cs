522e2f8730356b8ebac3bdf1070b6f42d669dd1b
Alexey_I <gafar@xaker.ru>
2023-02-02 08:38:58 +0300

    Itogovy script

--- Program.cs
+++ Program.cs
@@ -1,2 +1,23 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using System;
+ 
+namespace tretia_cifra
+{
+    class Program
+    {
+        static void Main()
+        {
+            Console.WriteLine("Введите число: ");
+            int chislo = Convert.ToInt32(Console.ReadLine());
+            string chislo_string = Convert.ToString(chislo);
+            int chislo_l = Convert.ToInt32(chislo_string.Length);
+            if (chislo_l < 3)
+            {
+                Console.WriteLine("В этом числе меньше трех знаков");
+            }
+            else
+            {
+                Console.WriteLine("Третья цифра этого числа: " + chislo_string[2]);
+            }
+        }
+    }
+}
